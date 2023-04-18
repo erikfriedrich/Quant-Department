af14fd995f6cc0fae8660acb0beb15bd8eb99bf0
erikfriedrich <45400909+erikfriedrich@users.noreply.github.com>
2023-04-18 17:00:42 +0200

    Update and rename 3. Session of QD: Relative Arbitrage V1.py to QD 3. Session: Relative Arbitrage.py

--- QD 3. Session: Relative Arbitrage.py
+++ QD 3. Session: Relative Arbitrage.py
@@ -3,16 +3,31 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 
+# display decimals to third place
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+# print ALL rows
+# (don't do it if your PC is slow)
 pd.set_option('display.max_rows', None)
 
 # read in excel file as our dataframe
+    # if you've forgotten how to do it, look into last sessions code
 df = pd.read_excel(r"/Users/erikfriedrich/Downloads/df_session3.xlsx")
 
 # make date column the index
 df = df.set_index('date')
 
-# make a new column with the relative value of gold to silver
+
+# Strategy:
+# compare relative value of gold and silver to historic average
+
+    # make a ceiling value
+        # if relative > avg+1,5*std we'll go short gold, long silver
+    # make a floor value
+        # if relative < avg-1,5*std we'll go long gold, short silver
+    # if the avg-std <= relative <= avg+std we'll (1) hold the s&p 500 or (2) go long silver and gold
+        # make a new column with the relative value of gold to silver
+        
 df["relative"] = df['gold']/df['silver']
 
 # make two new columns with the log returns of each asset
@@ -20,16 +35,28 @@ df['spy return'] = np.log(df['spy']).diff()
 df["gold return"] = np.log(df["gold"]).diff()
 df["silver return"] = np.log(df["silver"]).diff()
 
+# simple return if we go long gold with half our portfolio and short silver with the other half
+    # the one in the brackets indicates how many periods back we go
+        # try to experiment with different values
+        
 df['simple long gold'] = 0.5 * df['gold'].pct_change(1) - 0.5 * df['silver'].pct_change(1)
+
+# simple return if we go short gold with half our portfolio and long silver with the other
 df['simple short gold'] = (-0.5) * df['gold'].pct_change(1) + 0.5 * df['silver'].pct_change(1)
 
+# get the return if we're long both -> if the relative value of gold and silver is within our acceptible bandwith
 df['long both'] = 0.5*df['gold'].pct_change(1) + 0.5*df["silver"].pct_change(1)
-df["log long both"] = np.log(df['long both'] + 1)
 
+# turn all the simple returns into log returns
+df["log long both"] = np.log(df['long both'] + 1)
 df['log long gold'] = np.log(df['simple long gold'] + 1)
 df['log short gold'] = np.log(df['simple short gold'] + 1)
+
+# out of interest
 # calculate correlation between gold and silver ( around 0.9 )
 corr_gs = df['gold'].corr(df['silver'])
+
+# to get floor and ceiling
 
 # calculate the average gold to silver ratio [around 57.5]
 avg = df['relative'].mean(axis=0)
@@ -37,12 +64,7 @@ avg = df['relative'].mean(axis=0)
 # calculate the standard deviation of the gold/silver ratio [ around 19.9 ]
 std = df['relative'].std()
 
-# make a ceiling value
-    # if relative > avg+std we'll go short gold, long silver
-# make a floor value
-    # if relative < avg-std we'll go long gold, short silver
-# if the avg-std <= relative <= avg+std we'll hold the s&p 500
-
+# calculate floor and ceiling based on the avg relative value and the std of the relative values
 ceiling = avg+1.5*std
 floor = avg-1.5*std
 
@@ -51,14 +73,12 @@ df['signal'] = np.where(df["relative"] > ceiling, "short gold long silver", "lon
 df['signal'] = np.where(df["relative"] < floor, "long gold short silver", df['signal'])
 
 # get the returns of our trading strategy depending on the signal
-df['strategy'] = np.where(df['signal'] == "short gold long silver", (-1)*df['gold return'], df['spy return'])
-df['strategy'] = np.where(df['signal'] == "long gold short silver", (-1)*df['silver return'], df['strategy'])
 
-df['strategy2'] = np.where(df['signal'] == "short gold long silver", (-1)*df['gold return'], df['log long both'])
-df['strategy2'] = np.where(df['signal'] == "long gold short silver", (-1)*df['silver return'], df['strategy2'])
+df['strategy 1'] = np.where(df['signal'] == "short gold long silver", (-1)*df['gold return'], df['spy return'])
+df['strategy 1'] = np.where(df['signal'] == "long gold short silver", (-1)*df['silver return'], df['strategy'])
 
-df['strategy3'] = np.where(df['signal'] == "short gold long silver", df['log short gold'], df['log long both'])
-df['strategy3'] = np.where(df['signal'] == "long gold short silver", df['log long gold'], df['strategy3'])
+df['strategy 2'] = np.where(df['signal'] == "short gold long silver", df['log short gold'], df['log long both'])
+df['strategy 2'] = np.where(df['signal'] == "long gold short silver", df['log long gold'], df['strategy3'])
 
 # fill in NaN values with 0
 df.fillna(0, inplace = True)
@@ -67,20 +87,27 @@ df.fillna(0, inplace = True)
 df = df.truncate(before = '2000-01-01')
 
 # plotting the different strategies
-plt.plot(np.exp(df['strategy']).cumprod(), label = "return of our strategy")
+plt.plot(np.exp(df['strategy 1']).cumprod(), label = "return of our strategy 1 (with spy)")
 plt.plot(np.exp(df['spy return']).cumprod(), label = "return of spy")
-plt.plot(np.exp(df['strategy2']).cumprod(), label = 'Strategy 2')
-plt.plot(np.exp(df['strategy3']).cumprod(), label = 'Strategy 3')
+plt.plot(np.exp(df['strategy2']).cumprod(), label = 'return of our strategy 2 (without spy)')
 plt.legend(loc=2)
 plt.title("Our trading strategy against the S&P 500")
 plt.grid(True, alpha = .5)
 
-# calculate volatilities and plot them against eachother
+# calculate (daily) volatilities and plot them against eachother    
+    # what implications could the volatility of a strategy have for a Portfolio Manager?
+    
 df['Strategy Volatility'] = df['strategy'].rolling(window=252).std() * np.sqrt(252)
 df['S&P Volatility'] = df['spy return'].rolling(window=252).std() * np.sqrt(252)
-
 df[['Strategy Volatility', 'S&P Volatility']].plot(figsize=(8,6))
 
-# print correlation between our strategy and s&p 
+# print correlation between our strategy and s&p
+    # give me an interpretation of this value
 corr_strategy_spy = df['strategy'].corr(df['spy return'])
+
+
+
+
+
+
 print("correlation of our strategy to the spy:", corr_strategy_spy)
