012a1c1bf0f11ad2636a92d8158bd51f4fbebc56
erikfriedrich <45400909+erikfriedrich@users.noreply.github.com>
2023-09-17 16:05:28 +0200

    Update HWS 2023: Session 2 - Factor-Based Investing.py

--- HWS 2023: Session 2 - Factor-Based Investing.py
+++ HWS 2023: Session 2 - Factor-Based Investing.py
@@ -13,19 +13,16 @@ from pypfopt import expected_returns
 # initiate QuantBook
 qb = QuantBook()
 
-# get data for the factors: Quality, Momentum, Value, Size and Small Cap
-QUAL = qb.AddEquity("QUAL")
-MTUM = qb.AddEquity("MTUM")
-VLUE = qb.AddEquity("VLUE")
-SIZE = qb.AddEquity("SIZE")
-SVAL = qb.AddEquity("SVAL")
+# get data for the factors: Quality, Momentum, Value, Size, Small Cap and Emerging Markets
+equities = ["QUAL", "MTUM", "VLUE", "SIZE", "IJR", "IEMG"]
 
-history = qb.History(qb.Securities.Keys, 700, Resolution.Daily)
+for ticker in equities:
+    qb.AddEquity(ticker)
 
-# history or print(history) <- to see the dataframe in research.ipynb
+df = qb.History(qb.Securities.Keys, 2524, Resolution.Daily)["close"].unstack(level=0) # 2524 might seem weird but it maximizes our timeframe
 
-# unstack the df (as usual)
-df = history["close"].unstack(level=0)
+# df or print(df) <- to see the dataframe in research.ipynb
+
 
 # to use EfficientFrontier we have to provide the returns of each asset as well as the covariances
 returns = expected_returns.mean_historical_return(df)
@@ -37,7 +34,9 @@ ef = EfficientFrontier(returns, covariance, weight_bounds=(0.05, 0.50))
 raw_weightings= ef.max_sharpe(risk_free_rate=0.02)
 cleaned_weightings = ef.clean_weights()
 
+# print(raw_weights)
 # print(cleaned_weights)
+
 
 # pct change to calculate return
 df = df.pct_change()
@@ -46,13 +45,29 @@ df.fillna(0, inplace=True)
 # add a new column with the return of our portfolio
 df["portfolio simple return"] = sum(df[symbol] * cleaned_weightings[symbol] for symbol in cleaned_weightings.keys())
 
+# Add Spy for comparison
+SPY = qb.AddEquity("SPY").Symbol
+df["Spy"]= qb.History(SPY, 2524, Resolution.Daily)["close"].unstack(level=0).pct_change().fillna(0)
+df
+
 # Cumulative Return Plot
 cumulative_returns = (df[['portfolio simple return']] + 1).cumprod()
-plt.figure(figsize=(12, 6))
-cumulative_returns.plot(title='Cumulative Returns Over Time', ax=plt.gca())
+spy_returns = (df["Spy"] + 1).cumprod()
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+cumulative_returns.plot(ax=ax, label='Portfolio')
+spy_returns.plot(ax=ax, label='SPY')
+
+plt.legend()
+
+# Customize the plot
+ax.set_title('Cumulative Returns Over Time vs SPY')
 plt.grid(True, alpha=0.5)
 plt.xticks(rotation=45)
 plt.tight_layout()
+
+# Show the plot
 plt.show()
 
 # a. Volatility
