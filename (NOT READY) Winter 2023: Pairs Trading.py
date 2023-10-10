e78f6734e85f9977db29315638b8d11014fda622
erikfriedrich <45400909+erikfriedrich@users.noreply.github.com>
2023-10-11 00:47:51 +0200

    Update (NOT READY) Winter 2023: Pairs Trading.py

--- (NOT READY) Winter 2023: Pairs Trading.py
+++ (NOT READY) Winter 2023: Pairs Trading.py
@@ -1,56 +1,56 @@
 import pandas as pd 
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime
 from itertools import combinations
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import statsmodels.api as sm
 from math import floor
-from sklearn import linear_model
 from statsmodels.tsa.stattools import adfuller, coint
-import statsmodels
 
 qb = QuantBook()
 
-tickers = ['KO', 'PEP', 'ADBE', 'MSFT']
+# firstly, we'll need to get the following information about the stocks, we want to "investigate":
+        # (1) the stocks returns -> to calculate our performance in the end
+        # (2) the (log) price of the stocks -> to check for cointegration
+                # the linearization helps to detect cointegration
 
-start = datetime(2010,1,1)
-end = datetime(2018,1,1)
+def getStockInfo(tickers, start, end):
 
-results = pd.DataFrame()
+    # create a new DataFrame to store relevant informaition in
+    results = pd.DataFrame()
 
-for ticker in tickers:
-    symbol = qb.AddEquity(ticker).Symbol
-    df = qb.History(symbol, start, end, Resolution.Daily)["close"].unstack(level=0)
-    results[ticker] = df
+    # goes trough every ticker in our list of thickers
+    for ticker in tickers:
+        
+        # adds them as a stock symbol to our list of equities, so QuantBook can work with it
+        symbol = qb.AddEquity(ticker).Symbol
 
-stock_returns = results.pct_change().fillna(0)
-stock_prices = np.log(results)
+        # get daily price data for the ticker currently in, over our defined time periods (start -> end)
+        df = qb.History(symbol, start, end, Resolution.Daily)["close"].unstack(level=0)
 
-def stationary(data):
-    results_stationary = {}
+        # add the df referring to ONE ticker to our results dataframe as its own column
+        results[ticker] = df
 
-    for col in data.columns:
+    # calculate returns for each stock using pct_change() and fillna(0) [first values would be NaN]
+    stock_returns = results.pct_change().fillna(0)
 
-        test_result = adfuller(data[col])
-        p_value = test_result[1]
+    # turn results dataframe with "normal" prices into a dataframe of logarithmic prices
+    stock_prices = np.log(results)
 
-        if p_value <= 0.05:
-            results_stationary[col] = "stationary"
-        else:
-            results_stationary[col] = "non-stationary"
+    return stock_returns, stock_prices
 
-    return results_stationary
+# function that tests for cointegration between pairs of tickers
+def cointegration(data, tickers):
 
-stationary(spreads)
-
-def cointegration(data):
-
+    # we want to store the results in a dict, so we have to create one
     results_cointegration = {}
 
-    list_symbols  = data.columns
-    possible_pairs = list(combinations(list_symbols, 2))
+    # we define a list of symbols (list_symbols) that contains the column name of every 
+    #list_symbols  = data.columns 
+    possible_pairs = list(combinations(tickers, 2))
 
+    # Cointegration:
+            #  
     for pair in possible_pairs:
         score, p_value, _ = coint(data[pair[0]], data[pair[1]])
         
@@ -58,9 +58,6 @@ def cointegration(data):
             results_cointegration[pair] = "cointegrated"
 
     return results_cointegration
-
-results_cointegration = cointegration(stock_prices)
-results_cointegration
 
 def spread(ticker_pairs):
 
@@ -78,18 +75,15 @@ def spread(ticker_pairs):
         b = model.params[key[1]]
 
         spread[key] = y - b * x
-        spread[key].plot(figsize=(12,6))
-        plt.axhline(spread[key].mean(), color='black')
-        plt.legend(['Spread between ' + key[0] + " and " + key[1]])
-        plt.show()
+        
 
-        print(key[1])
-        print(key[0])
+        # uncomment to visualize each pairs spread
+        #spread[key].plot(figsize=(12,6))
+        #plt.axhline(spread[key].mean(), color='black')
+        #plt.legend(['Spread between ' + key[0] + " and " + key[1]])
+        #plt.show()
 
     return spread
-
-spreads = spread(results_cointegration)
-print(spreads)
 
 def z_score(spread_pairs):
 
@@ -98,19 +92,15 @@ def z_score(spread_pairs):
     for pair in spread_pairs.columns:
         
         z_scores[pair] = (spread_pairs[pair] - spread_pairs[pair].mean()) / spread_pairs[pair].std() 
-        z_scores[pair].plot(figsize=(12,6))
-        plt.axhline(z_scores[pair].mean())
-        plt.axhline(1.0, color='red')
-        plt.axhline(-1.0, color='green')
-        plt.show()
-        print(pair)
+        
+        # uncomment to visualize z_scores
+        #z_scores[pair].plot(figsize=(12,6))
+        #plt.axhline(z_scores[pair].mean())
+        #plt.axhline(1.0, color='red')
+        #plt.axhline(-1.0, color='green')
+        #plt.show()
         
     return z_scores
-
-
-z_scores = z_score(spreads)
-print(scores)
-
 
 def trade(stock_prices, spreads, z_scores, std_open, std_out, stock_returns):
 
@@ -135,11 +125,6 @@ def trade(stock_prices, spreads, z_scores, std_open, std_out, stock_returns):
 
     return strategy_returns
 
-std_open = 1
-std_out = 0.5
-returns = trade(stock_prices, spreads, z_scores, std_open, std_out, stock_returns)
-print(returns)
-
 def cumulative_returns(returns):
 
     cumulative_return = {}
@@ -152,4 +137,16 @@ def cumulative_returns(returns):
         
     return cumulative_return
 
+std_open = 1
+std_out = 0.5 # currently not used, but maybe in next weeks version
+tickers = ['KO', 'PEP', 'ADBE', 'MSFT']
+start = datetime(2010,1,1)
+end = datetime(2018,1,1)
+
+# assigns the returned dataframes from getStockInfo in the correct order to new dataframes (that we name the same, because they contain the same information and we're outside of the function now, so there won't be any interference)
+stock_returns, stock_prices = getStockInfo(tickers, start, end)
+results_cointegration = cointegration(stock_prices, tickers)
+spreads = spread(results_cointegration)
+z_scores = z_score(spreads)
+returns = trade(stock_prices, spreads, z_scores, std_open, std_out, stock_returns)
 cumulative_returns(returns)
