9cb59f4e2f0867a353019cd8816cdd913360f708
erikfriedrich <45400909+erikfriedrich@users.noreply.github.com>
2023-10-24 20:51:18 +0200

    Update Winter 2023: 4. Pairs Trading.py

--- Winter 2023: 4. Pairs Trading.py
+++ Winter 2023: 4. Pairs Trading.py
@@ -1,11 +1,10 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from datetime import datetime
 from itertools import combinations
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from math import floor
-from statsmodels.tsa.stattools import adfuller, coint
+from statsmodels.tsa.stattools import coint
 
 qb = QuantBook()
 
@@ -15,152 +14,198 @@ qb = QuantBook()
                 # the linearization helps to detect cointegration
 
 def getStockInfo(tickers, start, end):
+    """
+    Fetch daily closing prices for the specified tickers over the given date range.
 
-    # create a new DataFrame to store relevant informaition in
+    Parameters:
+    - tickers (list): List of stock tickers.
+    - start (datetime): Start date for the data retrieval.
+    - end (datetime): End date for the data retrieval.
+
+    Returns:
+    - stock_returns (DataFrame): Daily returns for each stock.
+    - stock_prices (DataFrame): Logarithmic prices for each stock.
+    """
+    # DataFrame to store the fetched data
     results = pd.DataFrame()
 
-    # goes trough every ticker in our list of thickers
+    # Loop through each ticker to retrieve data
     for ticker in tickers:
         try:
-            # adds them as a stock symbol to our list of equities, so QuantBook can work with it
+            # Add the ticker to QuantBook for data access
             symbol = qb.AddEquity(ticker).Symbol
 
-            # get daily price data for the ticker currently in, over our defined time periods (start -> end)
+            # Fetch daily closing prices for the ticker
             df = qb.History(symbol, start, end, Resolution.Daily)["close"].unstack(level=0)
 
             if df.empty:
                 print(f"No data available for {ticker} in the specified date range.")
                 continue
-            # add the df referring to ONE ticker to our results dataframe as its own column
+            
             results[ticker] = df
 
         except KeyError:
             print(f"Data for {ticker} not found.")
             continue
-        
-    if results.isnull().values.any():
-        print("WARNING, The DataFrame containts NaN value(s). The specified range is probably too long.")
-    # calculate returns for each stock using pct_change() and fillna(0) [first values would be NaN]
-    stock_returns = results.pct_change().fillna(0)
 
-    # turn results dataframe with "normal" prices into a dataframe of logarithmic prices
+    # Warning for missing values
+    if results.isnull().values.any():
+        print("WARNING: The DataFrame contains NaN value(s). The specified range might be too long.")
+
+    # Calculate daily stock returns and logarithmic prices
+    stock_returns = results.pct_change().fillna(0)
     stock_prices = np.log(results)
 
     return stock_returns, stock_prices
 
-# function that tests for cointegration between pairs of tickers
 def cointegration(data, tickers):
+    """
+    Check for cointegration between pairs of tickers.
 
-    # we want to store the results in a dict, so we have to create one
+    Parameters:
+    - data (DataFrame): Stock price data.
+    - tickers (list): List of tickers.
+
+    Returns:
+    - results_cointegration (dict): Pairs that are cointegrated.
+    """
     results_cointegration = {}
-
-    # we define a list of symbols (list_symbols) that contains the column name of every 
-    #list_symbols  = data.columns 
     possible_pairs = list(combinations(tickers, 2))
 
-    # Cointegration:
-            #  
     for pair in possible_pairs:
-        score, p_value, _ = coint(data[pair[0]], data[pair[1]])
+        _, p_value, _ = coint(data[pair[0]], data[pair[1]])
         
+        # A p-value below 0.05 suggests cointegration
         if p_value <= 0.05:
             results_cointegration[pair] = "cointegrated"
 
     print(results_cointegration)
-
     return results_cointegration
 
 def spread(ticker_pairs):
+    """
+    Calculate the spread between cointegrated stock pairs.
 
+    Parameters:
+    - ticker_pairs (dict): Cointegrated stock pairs.
+
+    Returns:
+    - spread (DataFrame): Spread values for each pair.
+    """
     spread = pd.DataFrame()
 
     for key in results_cointegration.keys():
-    
         x = stock_prices[key[1]]
         y = stock_prices[key[0]]
-        
+
+        # Use Ordinary Least Squares (OLS) for regression
         x = sm.add_constant(x)
         model = sm.OLS(y, x).fit()
-        
-        x = x[key[1]]
-        b = model.params[key[1]]
 
-        spread[key] = y - b * x
+        # Calculate spread
+        spread[key] = y - model.params[key[1]] * x[key[1]]
         
-
         # uncomment to visualize each pairs spread
         #spread[key].plot(figsize=(12,6))
         #plt.axhline(spread[key].mean(), color='black')
         #plt.legend(['Spread between ' + key[0] + " and " + key[1]])
         #plt.show()
 
+
     return spread
 
 def z_score(spread_pairs):
+    """
+    Calculate the z-score for spread of stock pairs.
 
+    Parameters:
+    - spread_pairs (DataFrame): Spread values for stock pairs.
+
+    Returns:
+    - z_scores (DataFrame): Z-scores for each pair.
+    """
     z_scores = pd.DataFrame()
 
     for pair in spread_pairs.columns:
-        
-        z_scores[pair] = (spread_pairs[pair] - spread_pairs[pair].mean()) / spread_pairs[pair].std() 
-        
+        # Calculate z-score
+        z_scores[pair] = (spread_pairs[pair] - spread_pairs[pair].mean()) / spread_pairs[pair].std()
+
         # uncomment to visualize z_scores
         #z_scores[pair].plot(figsize=(12,6))
         #plt.axhline(z_scores[pair].mean())
         #plt.axhline(1.0, color='red')
         #plt.axhline(-1.0, color='green')
         #plt.show()
-        
+
     return z_scores
 
 def trade(stock_prices, spreads, z_scores, std_open, std_out, stock_returns):
+    """
+    Determine trading signals based on z-scores.
 
+    Parameters:
+    - stock_prices (DataFrame): Logarithmic prices for each stock.
+    - spreads (DataFrame): Spread values for each pair.
+    - z_scores (DataFrame): Z-scores for each pair.
+    - std_open (float): Z-score threshold for trading signals.
+    - std_out (float): Not currently used.
+    - stock_returns (DataFrame): Daily returns for each stock.
+
+    Returns:
+    - strategy_returns (DataFrame): Strategy returns based on trading signals.
+    """
     strategy_returns = pd.DataFrame(index=stock_prices.index)
-    
+
     for pair in spreads.columns:
+        returns_for_pair = []
 
-            returns_for_pair = []
+        # Determine buy/sell/hold signals based on z-scores
+        for i in range(len(stock_prices)-1):
+            if z_scores[pair][i] < -std_open:
+                returns_for_pair.append(- stock_returns[pair[1]][i+1] + stock_returns[pair[0]][i+1])
+            elif z_scores[pair][i] > std_open:
+                returns_for_pair.append(+ stock_returns[pair[1]][i+1] - stock_returns[pair[0]][i+1])
+            else:
+                returns_for_pair.append(0)
 
-            for i in range(len(stock_prices)-1):
-
-                if z_scores[pair][i] < -std_open:
-                    returns_for_pair.append(- stock_returns[pair[1]][i+1] + stock_returns[pair[0]][i+1])
-
-                elif z_scores[pair][i] > std_open:
-                    returns_for_pair.append(+ stock_returns[pair[1]][i+1] - stock_returns[pair[0]][i+1])
-
-                else:
-                    returns_for_pair.append(0)
-
-            returns_for_pair.append(0)
-
-            strategy_returns[pair] = returns_for_pair
+        returns_for_pair.append(0)
+        strategy_returns[pair] = returns_for_pair
 
     return strategy_returns
 
 def cumulative_returns(returns):
+    """
+    Calculate cumulative returns for the strategy.
 
+    Parameters:
+    - returns (DataFrame): Strategy returns based on trading signals.
+
+    Returns:
+    - cumulative_return (dict): Cumulative returns for each stock pair.
+    """
     cumulative_return = {}
     pair_returns = pd.DataFrame()
 
     for pair in returns.columns:
-
+        # Calculate cumulative returns
         pair_returns[pair] = (1 + returns[pair]).cumprod() -1
         cumulative_return[pair] = pair_returns[pair].iloc[-1]
-        
+
     return cumulative_return
 
 def visualize_strategy_performance(strategy_returns):
     """
-    Visualize the performance of the strategy over time.
+    Plot cumulative returns for the strategy.
+
+    Parameters:
+    - strategy_returns (DataFrame): Strategy returns based on trading signals.
     """
-
     for pair in strategy_returns.columns:
-
         cumulative_portfolio_returns = (1 + strategy_returns[pair]).cumprod() - 1
-        
+
+        # Plot cumulative returns
         plt.figure(figsize=(15, 6))
-        plt.plot(cumulative_portfolio_returns.index, cumulative_portfolio_returns.values * 100)  # Convert to percentage
+        plt.plot(cumulative_portfolio_returns.index, cumulative_portfolio_returns.values * 100)
         plt.title(f"Strategy Cumulative Returns over Time for {pair}")
         plt.xlabel("Date")
         plt.ylabel("Cumulative Returns (%)")
@@ -168,45 +213,54 @@ def visualize_strategy_performance(strategy_returns):
         plt.grid(True)
 
 def compute_sharpe_ratio(returns, rfr, cumulative_returns):
+    """
+    Calculate Sharpe ratio for the strategy.
 
+    Parameters:
+    - returns (DataFrame): Strategy returns based on trading signals.
+    - rfr (float): Risk-free rate.
+    - cumulative_returns (dict): Cumulative returns for each stock pair.
+
+    Returns:
+    - results (dict): Sharpe ratio for each stock pair.
+    """
     results = {}
-    """
-    Calculate the Sharpe ratio of the strategy based on the returns.
-    """
-    for col in returns.columns:
 
+    for col in returns.columns:
         expected_portfolio_return = (cumulative_returns[col] + 1) ** (1/ (len(returns)/252)) - 1
         annual_volatility = returns[col].std() * np.sqrt(252)
 
+        # Compute the Sharpe ratio
         sharpe_ratio = (expected_portfolio_return - rfr) / annual_volatility
 
-        results[col] = sharpe_ratio 
-    
+        results[col] = sharpe_ratio
+
     return results
 
-rfr = 0.02
-std_open = 1
-std_out = 0.5 # currently not used, but maybe in next weeks version
-tickers = ['KO', 'PEP', 'ADBE', 'MSFT']
-start = datetime(2010,1,1)
-end = datetime(2018,1,1)
+# Define constants and parameters
+rfr = 0.02  # Risk-free rate
+std_open = 1  # Z-score threshold for opening positions
+std_out = 0.5  # Z-score threshold for closing positions (currently unused)
+tickers = ['KO', 'PEP', 'ADBE', 'MSFT']  # List of stock tickers to analyze
+start = datetime(2010,1,1)  # Start date for data retrieval
+end = datetime(2018,1,1)  # End date for data retrieval
 
-# assigns the returned dataframes from getStockInfo in the correct order to new dataframes (that we name the same, because they contain the same information and we're outside of the function now, so there won't be any interference)
+# Execute the trading strategy pipeline
 stock_returns, stock_prices = getStockInfo(tickers, start, end)
 results_cointegration = cointegration(stock_prices, tickers)
 spreads = spread(results_cointegration)
 z_scores = z_score(spreads)
 returns = trade(stock_prices, spreads, z_scores, std_open, std_out, stock_returns)
-cum_return = cumulative_returns(returns)    
+cum_return = cumulative_returns(returns)
 visualize_strategy_performance(returns)
 sharpe_ratios = compute_sharpe_ratio(returns, rfr, cum_return)
 
-# Displaying cumulative returns
+# Display cumulative returns for each stock pair
 print("Cumulative Returns:")
 for pair, ret in cum_return.items():
     print(f"{pair[0]} and {pair[1]}: {ret:.2%}")
 
-# Display each strategies Sharpe-Ratio
-print("Sharpe Rations:")
+# Display Sharpe ratio for each stock pair
+print("Sharpe Ratios:")
 for pair, sr in sharpe_ratios.items():
-    print(f"{pair}: {sr:.2}")
+    print(f"{pair}: {sr:.2f}")
